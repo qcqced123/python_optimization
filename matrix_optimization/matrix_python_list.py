c03e6b6e2eb80ea185ab12f49e667bd0fc62641a
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-09-12 00:11:58 +0900

    Add day 1 study

---
+++ matrix_optimization/matrix_python_list.py
@@ -0,0 +1,54 @@
+import time
+"""
+All of source code based on <High Performance Python>
+"""
+
+try:
+    profile
+except NameError:
+    profile = lambda x: x
+
+grid_shape = (640, 640)
+
+
+@profile
+def evolve(grid, dt, D=1.0):
+    """
+    Args:
+         grid: global space of simulations
+         dt: derivative result
+
+    """
+    xmax, ymax = grid_shape  # because of assignment from global namespace
+    new_grid = [[0.0 for x in range(grid_shape[1])] for x in range(grid_shape[0])]
+    for i in range(xmax):
+        for j in range(ymax):
+            grid_xx = (
+                grid[(i + 1) % xmax][j] + grid[(i - 1) % xmax][j] - 2.0 * grid[i][j]
+            )
+            grid_yy = (
+                grid[i][(j + 1) % ymax] + grid[i][(j - 1) % ymax] - 2.0 * grid[i][j]
+            )
+            new_grid[i][j] = grid[i][j] + D * (grid_xx + grid_yy) * dt
+    return new_grid
+
+
+def run_experiment(num_iterations):
+    """ 2D-space distribution equation """
+    # setting up initial conditions
+    grid = [[0.0 for x in range(grid_shape[1])] for x in range(grid_shape[0])]
+
+    block_low = int(grid_shape[0] * 0.4)
+    block_high = int(grid_shape[0] * 0.5)
+    for i in range(block_low, block_high):
+        for j in range(block_low, block_high):
+            grid[i][j] = 0.005
+
+    start = time.time()
+    for i in range(num_iterations):
+        grid = evolve(grid, 0.1)
+    return time.time() - start
+
+
+if __name__ == "__main__":
+    run_experiment(500)
